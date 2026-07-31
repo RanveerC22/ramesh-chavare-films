0b12a5183a10497e34ccefec5d826ec0605459c0
Ranveer Chavare <ranveer.chavare@wolterskluwer.com>
2026-07-31 22:20:46 +0530

    Updated the contact details and social media links

--- data/siteConfig.ts
+++ data/siteConfig.ts
@@ -17,37 +17,37 @@ export const siteConfig = {
   yearsOfExperience: new Date().getFullYear() - 2014,
 
   contact: {
-    phonePrimary: "+91 98765 43210",
-    phonePrimaryRaw: "919876543210",
-    phoneSecondary: "+91 91234 56789",
-    phoneSecondaryRaw: "919123456789",
-    whatsappNumber: "919876543210",
-    email: "info@rameshchavarefilms.com",
+    phonePrimary: "9763978008",
+    phonePrimaryRaw: "9763978008",
+    phoneSecondary: "9175333400",
+    phoneSecondaryRaw: "9175333400",
+    whatsappNumber: "9763978008",
+    email: "kolhapurfilmcamerarentals@gmail.com",
     address: {
-      line1: "Shop No. 4, Station Road",
-      line2: "Near Rajarampuri, Kolhapur",
+      line1: "A/22/7-8, Rajlaxmi Nagar, Devkar Panand",
+      line2: "Near Nikam Park, Kolhapur",
       city: "Kolhapur",
       state: "Maharashtra",
-      pincode: "416001",
+      pincode: "416012",
       country: "India",
     },
     mapEmbedUrl:
       "https://www.google.com/maps?q=Kolhapur,Maharashtra&output=embed",
     mapLinkUrl: "https://maps.google.com/?q=Kolhapur,Maharashtra",
     hours: [
-      { day: "Monday – Saturday", time: "9:30 AM – 8:30 PM" },
+      { day: "Monday – Saturday", time: "9:30 AM – 9:30 PM" },
       { day: "Sunday", time: "10:00 AM – 2:00 PM (Pickup/Drop only)" },
     ],
   },
 
   social: {
-    instagram: "https://instagram.com/rameshchavarefilms",
+    instagram: "https://instagram.com/rameshc_filmcamerarentals",
     youtube: "https://youtube.com/@rameshchavarefilms",
-    facebook: "https://facebook.com/rameshchavarefilms",
+    facebook: "https://facebook.com/share/1CoqyXwjBj/",
   },
 
   whatsappDefaultMessage:
-    "Hi Ramesh Chavare Films, I'd like to know more about your equipment rentals.",
+    "Hi Ramesh Chavare Films, I'd like to enquire more about some equipment rentals.",
 
   rateCardPdfUrl: "/rate-card.pdf",
 };
